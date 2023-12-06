13b058f6675ffb081ba470d13938aa8391f0b348
Walter Bernal <wltrbm@gmail.com>
2023-12-06 15:34:37 -0700

    indentation update

--- migrations/2_contracts_migration.js
+++ migrations/2_contracts_migration.js
@@ -2,6 +2,6 @@ const erc20Token = artifacts.require("./erc20Token.sol");
 const SupplyChain = artifacts.require("./SupplyChain.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(erc20Token, 10000, "TotalSem Token", 18, "TotalSem");
-    deployer.deploy(SupplyChain);
+  deployer.deploy(erc20Token, 10000, "TotalSem Token", 18, "TotalSem");
+  deployer.deploy(SupplyChain);
 };
